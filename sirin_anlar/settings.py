e97bc3abe59e6ed8f90b9342d472b4fff9a1e555
Eldaraze12 <eldarvalorant123@gmail.com>
2026-07-12 23:56:21 +0400

    fix: STORAGES ayarlarina default eklendi

--- sirin_anlar/settings.py
+++ sirin_anlar/settings.py
@@ -94,11 +94,9 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STORAGES = {
-  
     'staticfiles': {
         'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
     },
-    # 2. Resim ve dosya yüklemeleri için eksik olan varsayılan ayar (EKLENECEK)
     'default': {
         'BACKEND': 'django.core.files.storage.FileSystemStorage',
     },
